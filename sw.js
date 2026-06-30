7e1eba2847f743e1bed8fe79033d6104a5737a9a
ghkdk789-prog <ghkdk789@gmail.com>
2026-07-01 03:57:51 +0500

    Обновить кэш архива после добавления базы

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'broken-soul-archive-v1';
-const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+const CACHE = 'broken-soul-archive-v2';
+const ASSETS = ['./','./index.html','./styles.css','./app.js','./data-scenes.js','./data-characters.js','./data-world.js','./manifest.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(resp => { const copy = resp.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return resp; }).catch(() => caches.match('./index.html')))));
